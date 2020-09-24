018e96cc420088b7bfe209d5fd8858268b2e55f9
YIPYU <yipyu19@gmail.com>
2020-09-24 15:48:15 +0800

    calc avg reviews

--- reviews.py
+++ reviews.py
@@ -6,8 +6,13 @@ with open('111.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0:
 		    print(len(data))
+print('檔案讀取完了，總共有',len(data),'筆資料')
+# print(len(data))
+# print(data[0])
+# print('-----------------------------------')
+# print(data[1])
 
-print(len(data))
-print(data[0])
-print('-----------------------------------')
-print(data[1])
+sum_len = 0
+for d in data:
+    sum_len = sum_len + len(d)
+print('每筆的平均長度爲',sum_len/len(data))
